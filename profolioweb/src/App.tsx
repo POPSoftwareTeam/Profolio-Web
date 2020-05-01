7f080436c5f0bdfdb21c25eb397366e4764168ea
Kyler <kyler@pop-os.localdomain>
2020-04-30 19:49:45 -0600

    This isnt working. the form is there but it is like none of the api things even get called

--- profolioweb/src/App.tsx
+++ profolioweb/src/App.tsx
@@ -1,30 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import APIAuthenticationService from "./Services/APIAuthenticationService"
+import APIAuthenticationService from './Services/APIAuthenticationService';
+import APIPhotoService from './Services/APIPhotoService';
 import { User } from './Models/UserModel';
 
-function App() {
-  let authservice = new APIAuthenticationService()
-  authservice.Login(new User("kyler.daybell96@gmail.com","kyler"))
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App:React.FC = props => {
+    let [image, setImage] = React.useState<File|null>();
+
+
+    const submitCharityToAPI = async () => {
+      let authservice = new APIAuthenticationService()
+      let token =  await authservice.Login(new User("kyler.daybell96@gmail.com","kyler"))
+      if(token){
+        let photoservice = new APIPhotoService();
+        await photoservice.UploadPhoto(token,image);
+        console.log(image);
+      }
+    }
+    
+    return (
+      <>
+        <form>
+            <h3>PhotoUpload</h3>
+            <input type="file" onChange={(event:any) => setImage(event.target.files[0])}></input>
+            <input type="submit" onClick={async ()=> await submitCharityToAPI()}/>
+        </form>
+       </>
+    )
 }
+
+
+
 
 export default App;
---
+++ profolioweb/src/Services/APIPhotoService.ts
@@ -0,0 +1,24 @@
+import { Token } from "../Models/TokenModel";
+const fs = require('fs')
+
+export default class APIPhotoService{
+    readonly api = "http://206.189.218.168"
+    public async UploadPhoto(token:Token,file:any){
+        console.error("in the fetch")
+        let newurl = this.api+"/Photos/Upload"
+        let formData = new FormData()
+        formData.append('avatar',file,'image.jpg')
+        let headers = { 
+            "Content-Type": "multipart/form-data", 
+            "Authorization": "Bearer "+token.key
+        };
+        let response = await fetch(newurl, { 
+            method: "POST",
+            mode: "cors",
+            headers: headers,
+            body: formData
+        })
+        let responseJson = await response.json();
+        console.log(responseJson)
+    }
+}
