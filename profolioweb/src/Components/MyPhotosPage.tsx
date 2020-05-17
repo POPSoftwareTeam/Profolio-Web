8c2e29d21e3a7dd0516a3f52fd1d3d7d476ff0f7
Kyler <kyler@pop-os.localdomain>
2020-05-17 12:00:07 -0600

    you can now view the photos that you uploaded

--- profolioweb/src/Components/MyPhotosPage.tsx
+++ profolioweb/src/Components/MyPhotosPage.tsx
@@ -4,27 +4,31 @@ import APIPhotoService from '../Services/APIPhotoService';
 
 
 const MyPhotos:React.FC = props => {
-    let [myImages] = React.useState<[string]>([""]);
+    let [myImages,setMyImages] = React.useState<[string]>([""]);
+    //let myImages:[string] = [""];
     useEffect(() => {
         const setup = async () => {
             let iphotoservice = new APIPhotoService;
             let photos = await iphotoservice.GetMyPhotos();
+            let images:[string] = [""];
             for(let item in photos){
                 let newimage = await iphotoservice.GetLowResPhoto(photos[item])
                 if(newimage){
-                    myImages.push(newimage);
+                    images.push(newimage);
                 }
             }
-            myImages.shift();
-            console.log(myImages)
+            images.shift();
+            setMyImages(images)
         };
         setup();
-    });
+    },[]);
     
     return (
       <>
         <h1>Your Photos</h1>
-        <img src={myImages[0]}/>
+        {myImages.map((image2, index) => (
+            <img src={image2} key={index} />
+          ))}
        </>
     )
 }
