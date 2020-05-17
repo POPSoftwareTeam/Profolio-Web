9e49b63e35ba5337f134a311feb3d06e6fbec8ea
Kyler <kyler@pop-os.localdomain>
2020-05-17 15:18:27 -0600

    still not working

--- profolioweb/src/Components/shared/PhotoCard.tsx
+++ profolioweb/src/Components/shared/PhotoCard.tsx
@@ -5,12 +5,13 @@ import { Photo } from '../../Models/PhotoModel';
 
 
 export const PhotoCard: FunctionComponent<any> = (props:Photo) => {
-    let [username,setUsername] = React.useState<[string]>([""]);
     const shareImage = function(props:Photo){
         
     }
     return(                
     <>
+        {console.log(props.base64file)}
+        {console.log(props.imagename)}
         <img src={props.base64file}  alt={props.imagename}/>
         <div>
             <div className="button" onClick={()=>shareImage(props)}>share this image</div>
