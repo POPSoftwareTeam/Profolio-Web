9cf6066d221cfeed95c1a9f8acfce9cdcc3eb5c2
Kyler Daybell <kyler.daybell96@gmail.com>
2020-05-19 13:36:43 -0600

    updated the shared photos page to look like the myphotos page

--- profolioweb/src/Components/SharedPhotosPage.tsx
+++ profolioweb/src/Components/SharedPhotosPage.tsx
@@ -6,27 +6,36 @@ import {Photo} from "../Models/PhotoModel"
 
 
 const SharedPhotos:React.FC = props => {
-    let [myImages,setMyImages] = React.useState<[Photo]>([new Photo("flip","off")]);
+    let [myImages,setMyImages] = React.useState<[string]>([""]);
+    let [ready,setReady] = React.useState<boolean>(false)
     useEffect(() => {
         const setup = async () => {
             let iphotoservice = new APIPhotoService;
-            var photos = await iphotoservice.GetSharedPhotos();
-            setMyImages(photos)
-            console.log("Photos",photos)
+            var photos = await iphotoservice.GetSharedPhotoNames();
+            await myImages.push(...photos);
             console.log("myImages",myImages)
-
+            myImages.shift();
+            setReady(true)
         };
         setup();
     },[]);
     
-    return (
-      <>
-        <h1>Your Photos</h1>
-        {myImages.map((i,key) => (
-            <PhotoCard key={key} photo={i} />
-          ))}
-       </>
-    )
+    if(!ready){
+        return(
+            <h1>LOADING....</h1>
+        )
+    }else{
+        return (
+          <>
+            <h1>Shared With You</h1>
+            <div className = "photo-holder">
+                {myImages.map((key,i) => (
+                    <PhotoCard key={key} photo={myImages[i]} />
+                    ))}
+            </div>
+           </>
+        )
+    }
 }
 
 
