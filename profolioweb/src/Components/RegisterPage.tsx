777f2ec58a08954b53cd84875fc8ee2f6c1f4964
Mike <mexvance04@gmail.com>
2020-05-25 16:38:53 -0600

    register page has some password validation now, we can change it but the effect is cool, we should update it to a better standard of updating vs what im doing, and also hide it when we aren't using it

--- profolioweb/src/Components/RegisterPage.tsx
+++ profolioweb/src/Components/RegisterPage.tsx
@@ -10,9 +10,20 @@ const Register: React.FC<RouteComponentProps>= (props) => {
   let [email, setEmail] = React.useState<string>("");
   let [password, setPassword] = React.useState<string>("");
   let [verifyPassword, setVerifyPassword] = React.useState<string>("");
+  let validations:number[] = [
+        (password.length >= 5)?1:0, 
+        (password.search(/[A-Z]/) > -1)?1:0, 
+        (password.search(/[0-9]/) > -1)?1:0, 
+        (password.search(/[$&+,:;=?@#]/) > -1) ?1:0
+    ];
+  let strength: number = validations.reduce(function(a, b){return a + b});;
 
+   const validatePassword = (event:any) => {
+    setPassword(event);
+  }
   const submitCharityToAPI = async () => {
     let authservice = new APIAuthenticationService();
+    
     if (password == verifyPassword) {
       let response: boolean;
       response = await authservice.Register(new User(email, password));
@@ -45,15 +56,29 @@ const Register: React.FC<RouteComponentProps>= (props) => {
               <label  className="label"><div className="content">Email:</div></label>
               </div>
               <div className="formContent">
-              <input
-                type="password"
-                className="pwd input"
-                placeholder=" "
-                value={password}
-                onChange={(event: any) => setPassword(event.target.value)}
-              />
-              <label  className="label"><div className="content">Password:</div></label>
+                <input
+                  type="password"
+                  className="pwd input"
+                  placeholder=" "
+                  value={password}
+                  onChange={(event: any) => validatePassword(event.target.value)}
+                />
+                <label  className="label"><div className="content">Password:</div></label>
+                <div className="strength">
+                  <span className={strength > 0 ? "bar bar-1":"bar bar-white"} />
+                  <span className={strength > 1 ? "bar bar-2":"bar bar-white"} />
+                  <span className={strength > 2 ? "bar bar-3":"bar bar-white"} />
+                  <span className={strength > 3 ? "bar bar-4":"bar bar-white"} />
+                </div>
+                <div  className="validation">
+                {console.log(validations[0])}
+                  <div> {validations[0] === 1 ? '✔️' : '❌'} must be at least 5 characters</div>
+                  <div> {validations[1] === 1 ? '✔️' : '❌'} must contain a capital letter</div>
+                  <div> {validations[2] === 1 ? '✔️' : '❌'} must contain a number</div>
+                  <div> {validations[3] === 1 ? '✔️' : '❌'} must contain one of $&+,:;=?@#</div>
+                </div>
               </div>
+             
               <div className="formContent">
               <input
                 type="password"
